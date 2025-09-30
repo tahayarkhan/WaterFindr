723ec8a94686e29bfc70fb6469ceb1eef5ca0e4d
tahayarkhan <tahayarkhan03@gmail.com>
2025-09-30 12:59:35 -0400

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white text-center p-4">
+      <h1 className="text-5xl font-extrabold text-blue-500 mb-4">
+        WaterFindr
+      </h1>
+    </div>
+  );
+}
+
+export default App
